6ae27256f7ca913cdba1cdbdd4944d9e6df441b5
joaofelipe <joaofelipe1294@gmail.com>
2022-11-27 11:16:34 -0300

    chore: remove excoveralls

--- mix.exs
+++ mix.exs
@@ -7,14 +7,7 @@ defmodule ExmonGame.MixProject do
       version: "0.1.0",
       elixir: "~> 1.12",
       start_permanent: Mix.env() == :prod,
-      test_coverage: [tool: ExCoveralls],
-      deps: deps(),
-      preferred_cli_env: [
-        coveralls: :test,
-        "coveralls.detail": :test,
-        "coveralls.post": :test,
-        "coveralls.html": :test
-      ]
+      deps: deps()
     ]
   end
 
@@ -28,7 +21,6 @@ defmodule ExmonGame.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:excoveralls, "~> 0.10", only: :test},
     ]
   end
 end
