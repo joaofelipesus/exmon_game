185123c650abb777cd65aef91b7bc2165411b000
joaofelipe <joaofelipe1294@gmail.com>
2022-11-27 10:47:14 -0300

    Initial commit

---
+++ lib/exmon_game.ex
@@ -0,0 +1,18 @@
+defmodule ExmonGame do
+  @moduledoc """
+  Documentation for `ExmonGame`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ExmonGame.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/exmon_game_test.exs
@@ -0,0 +1,8 @@
+defmodule ExmonGameTest do
+  use ExUnit.Case
+  doctest ExmonGame
+
+  test "greets the world" do
+    assert ExmonGame.hello() == :world
+  end
+end
