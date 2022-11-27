9c4a8df579b87f8182fce4ee1439fa777ec9fc72
joaofelipe <joaofelipe1294@gmail.com>
2022-11-27 14:36:26 -0300

    feat: add update life points methods

--- lib/player.ex
+++ lib/player.ex
@@ -16,4 +16,19 @@ defmodule Player do
   def cure_power do
     Enum.random(18..25)
   end
+
+  def update_life_points(player, value, :decrease) do
+    new_life_points = player.life_points - value
+
+    cond do
+      new_life_points < 0 -> {:ok, %{player | life_points: 0}}
+      new_life_points >= 0 -> {:ok, %{player | life_points: new_life_points}}
+    end
+  end
+
+  def update_life_points(player, value, :increase) do
+    player = %{player | life_points: player.life_points + value}
+
+    {:ok, player}
+  end
 end
--- test/player_test.exs
+++ test/player_test.exs
@@ -16,7 +16,7 @@ defmodule PlayerTest do
     assert player.life_points == 100
   end
 
-  describe "default_attack" do
+  describe "default_attack/0" do
     test "it returns a number between min and max default attack value" do
       attack_value = Player.default_attack()
       assert attack_value >= 18
@@ -24,7 +24,7 @@ defmodule PlayerTest do
     end
   end
 
-  describe "special_attack" do
+  describe "special_attack/0" do
     test "it returns a number between min and max default attack value" do
       attack_value = Player.special_attack()
       assert attack_value >= 10
@@ -32,11 +32,50 @@ defmodule PlayerTest do
     end
   end
 
-  describe "cure_power" do
+  describe "cure_power/0" do
     test "it returns a number between min and max default attack value" do
       attack_value = Player.cure_power()
       assert attack_value >= 18
       assert attack_value <= 25
+    end
+  end
+
+  describe "update_life_points/3" do
+    test "it returns a player with increased life_points when receive increase" do
+      player = %Player{
+        name: "José Aldo Jr.",
+        default_attack_name: "jeb",
+        special_attack_name: "low kick",
+        cure_power_name: "Dedé speach"
+      }
+
+      {:ok, player} = Player.update_life_points(player, 15, :increase)
+      assert player.life_points == 115
+    end
+
+    test "it returns a player with decreased life_points when receive decrease" do
+      player = %Player{
+        name: "José Aldo Jr.",
+        default_attack_name: "jeb",
+        special_attack_name: "low kick",
+        cure_power_name: "Dedé speach"
+      }
+
+      {:ok, player} = Player.update_life_points(player, 20, :decrease)
+      assert player.life_points == 80
+    end
+
+    test "it returns when receive decrease and a value bigger than life_points" do
+      player = %Player{
+        name: "José Aldo Jr.",
+        default_attack_name: "jeb",
+        special_attack_name: "low kick",
+        cure_power_name: "Dedé speach",
+        life_points: 15
+      }
+
+      {:ok, player} = Player.update_life_points(player, 20, :decrease)
+      assert player.life_points == 0
     end
   end
 end
