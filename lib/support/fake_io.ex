975033be38e00671fb78d199552959ab3dad1935
joaofelipe <joaofelipe1294@gmail.com>
2022-11-27 21:58:09 -0300

    fix: remove fake_io module

--- lib/support/fake_io.ex
+++
@@ -1,9 +0,0 @@
-defmodule FakeIO do
-  @doc """
-  This module is used only as test support, it mocks human user inputs.
-  """
-  def gets("Name: "), do: "José"
-  def gets("Default attack: "), do: "Jeb"
-  def gets("Special attack: "), do: "Low kick"
-  def gets("Cure power: "), do: "Rest"
-end
