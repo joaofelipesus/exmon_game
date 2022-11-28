943c5cfd47db3e51cba3560ba84972141de4ae66
joaofelipe <joaofelipe1294@gmail.com>
2022-11-27 22:06:16 -0300

    feat: remove deprecated function print/3 and fix specs.

--- lib/turn.ex
+++ lib/turn.ex
@@ -1,13 +1,15 @@
 defmodule Turn do
-  def print(player, action, value) do
-    IO.puts("#{player.name}  -  #{formatted_action(action)}  -  #{value}")
+  def print(perform_player, receive_player, action, value) do
+    IO.puts(
+      "Performed: #{perform_player.name} | Received: #{receive_player.name} |  #{formatted_action(action)} |  #{value}"
+    )
   end
 
   defp formatted_action(action) do
     case action do
       :default_attack -> "#{IO.ANSI.yellow()}default attack#{reset_color()}"
       :special_attack -> "#{IO.ANSI.red()}special attack#{reset_color()}"
-      :cure -> "#{IO.ANSI.green()}cure#{reset_color()}"
+      :cure_power -> "#{IO.ANSI.green()}cure power#{reset_color()}"
     end
   end
 
--- test/turn_test.exs
+++ test/turn_test.exs
@@ -4,30 +4,32 @@ defmodule TurnTest do
   import ExUnit.CaptureIO
 
   setup do
-    player = %Player{
-      name: "asuka",
-      special_attack_name: "kick",
-      default_attack_name: "scream",
-      cure_power_name: "sleep"
-    }
+    human_player = Player.new_player(:human)
+    bot_player = Player.new_player(:bot)
 
-    {:ok, player: player}
+    {:ok, human_player: human_player, bot_player: bot_player}
   end
 
-  describe "print/3" do
+  describe "print/4" do
     test "prints green value when cure", context do
-      assert capture_io(fn -> Turn.print(context[:player], :cure, 15) end) ==
-               "asuka  -  \e[32mcure\e[37m  -  15\n"
+      assert capture_io(fn ->
+               Turn.print(context[:human_player], context[:bot_player], :cure_power, 15)
+             end) ==
+               "Performed: Leon S. Kennedy | Received: Nemesis |  \e[32mcure power\e[37m |  15\n"
     end
 
     test "prints yellow value when default attack", context do
-      assert capture_io(fn -> Turn.print(context[:player], :default_attack, 15) end) ==
-               "asuka  -  \e[33mdefault attack\e[37m  -  15\n"
+      assert capture_io(fn ->
+               Turn.print(context[:human_player], context[:bot_player], :default_attack, 15)
+             end) ==
+               "Performed: Leon S. Kennedy | Received: Nemesis |  \e[33mdefault attack\e[37m |  15\n"
     end
 
     test "prints red value when special attack", context do
-      assert capture_io(fn -> Turn.print(context[:player], :special_attack, 15) end) ==
-               "asuka  -  \e[31mspecial attack\e[37m  -  15\n"
+      assert capture_io(fn ->
+               Turn.print(context[:human_player], context[:bot_player], :special_attack, 15)
+             end) ==
+               "Performed: Leon S. Kennedy | Received: Nemesis |  \e[31mspecial attack\e[37m |  15\n"
     end
   end
 end
